c6832b7ada8dee8213550b246aebaacb2fc00750
Courtney Frey <courtneyb.frey@gmail.com>
2023-01-27 16:50:36 -0600

    routing logic added to the mvc project

---
+++ HeyThereWorld/Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HeyThereWorld.Controllers
+{
+    [Route("/helloworld/")]
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method = 'post' action = 'helloworld/'>" +
+                "<input type = 'text' name = 'name' />" +
+                "<input type = 'submit' value = 'Greet Me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        //GET route: /hello/welcome
+        [HttpGet("welcome/{name?}")]
+        [HttpPost]
+        public IActionResult Welcome (string name = "World")
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+    }
+}
+
---
+++ HeyThereWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HeyThereWorld.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
